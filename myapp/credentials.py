9b9637f6128b1b0ee4b0b6b157e2edd28b94d280
M4ks123 <161470983+M4ks123@users.noreply.github.com>
2024-04-06 00:20:52 +0300

    Add files via upload

---
+++ myapp/credentials.py
@@ -0,0 +1,9 @@
+TOKEN = '7019777911:AAHXaaMkf_lw7gCWd5L3OSgu3adoBuGVw2w'
+TELEGRAM_API_URL = f'https://api.telegram.org/bot{TOKEN}/'
+URL = 'https://e377-193-106-145-223.ngrok-free.app/getpost/'
+HOSTDB='mysql-16c26c33-ostrovskii-ca5d.a.aivencloud.com'
+DBNAME='defaultdb'
+PORTDB=22031
+USERDB='avnadmin'
+PASSDB='AVNS_Ad0t6G0hRdW5J0tuKRZ'
+TIMEOUT=10
